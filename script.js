e99909e6c962b6f5aad7b3bdbf3a96cddff0f6e3
Raj Pandey <rajpandey552002@gmail.com>
2023-10-06 18:09:26 +0530

    Added Menu Fade Animation

--- script.js
+++ script.js
@@ -117,3 +117,24 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+//Menu Fade Animation
+
+const nav = document.querySelector('.nav');
+
+const handlehover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handlehover.bind(0.5));
+
+nav.addEventListener('mouseout', handlehover.bind(1.0));
